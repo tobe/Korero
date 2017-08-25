7f9e501c683d588b6fb68110e556de73d92759eb
infyhr <infy@cybershade.org>
2017-08-25 03:32:46 +0200

    Added Webpack suite (Angular, Bootstrap, jQ, ...)

---
+++ Korero/ClientApp/vendors.ts
@@ -0,0 +1,7 @@
+import 'jquery/dist/jquery';
+import 'bootstrap/dist/js/bootstrap.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Add etc here
+//import 'ng2-slim-loading-bar';
+//import 'ng2-slim-loading-bar/style.css';
---
+++ Korero/README.md
@@ -0,0 +1,3 @@
+# Korero
+
+`./node_modules/webpack/bin/webpack.js --watch --progress --colors --config webpack.development.js`
