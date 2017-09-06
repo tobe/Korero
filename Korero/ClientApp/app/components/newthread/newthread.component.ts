396817487e354b0fac1af475dfaca362ce758514
infyhr <infy@cybershade.org>
2017-09-07 01:00:56 +0200

    Completed the frontend (more or less) for the new thread component.

--- Korero/ClientApp/app/components/newthread/newthread.component.ts
+++ Korero/ClientApp/app/components/newthread/newthread.component.ts
@@ -1,28 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ThreadService } from '../../services/thread.service';
 import { ReplyService } from '../../services/reply.service';
+import { TagService } from '../../services/tag.service';
 
 import { Thread } from '../../models/thread';
 import { Reply } from '../../models/reply';
+import { Tag } from '../../models/tag';
 
 import { NotificationsService } from 'angular2-notifications';
 
 @Component({
     selector: 'app-newthread',
     templateUrl: './newthread.component.html',
-    providers: [ThreadService, ReplyService]
+    styleUrls: ['./newthread.component.css'],
+    providers: [ThreadService, ReplyService, TagService]
 })
-export class NewThreadComponent {
+export class NewThreadComponent implements OnInit {
     public reply: Reply = new Reply();
     public thread: Thread = new Thread();
+    public tags: Tag[];
 
     constructor(
         private replyService: ReplyService,
         private threadService: ThreadService,
+        private tagService: TagService,
         private notificationService: NotificationsService
     ) {}
 
+    ngOnInit() {
+        // Fetch all the tags
+        this.getTags();
+    }
 
+    getTags(): void {
+        this.tagService.getTags().then(tags => this.tags = tags);
+    }
 
 }
