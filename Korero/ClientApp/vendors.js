a443c77ed6ffa9182d9f942901a7fbc88a57d70b
infyhr <infy@cybershade.org>
2018-02-19 20:35:58 +0100

    fixed untracked files

--- Korero/ClientApp/vendors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("jquery/dist/jquery");
-require("popper.js/dist/popper.min");
-require("bootstrap/dist/js/bootstrap.min");
-require("bootstrap/dist/css/bootstrap.min.css");
-//# sourceMappingURL=vendors.js.map
