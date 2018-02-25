f8bad5016a564837fcbb86398479024455ae2b58
infyhr <infy@cybershade.org>
2018-02-25 13:59:03 +0100

    Fix registration so it works with a username

--- Korero/Controllers/AccountController.cs
+++ Korero/Controllers/AccountController.cs
@@ -94,7 +94,7 @@ namespace Korero.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
